15f2a1ee84c2ccf901e65b75ab0d44b0903609c9
rushikesh Pawar <rushikesh.pawarms@gmail.com>
2021-10-14 17:12:25 +0530

    adding front end to tools module

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let canvas = document.querySelector("#canvas");
+
 let ctx = canvas.getContext("2d");
 let mouseD=false;
 // canvas.height=window.innerHeight-100;
 // canvas.width=window.innerWidth;
 let { top: canvasTop } = canvas.getBoundingClientRect();
-
-
 canvas.height = window.innerHeight - canvasTop;
 canvas.width = window.innerWidth;
 window.addEventListener("resize", function () {
@@ -13,15 +12,12 @@ window.addEventListener("resize", function () {
     canvas.width = window.innerWidth;
 })
 canvas.addEventListener("mousedown", function(e){
-
     mouseD=true;
     let x=e.clientX;
     let y=e.clientY-canvasTop;
     ctx.beginPath();
-    ctx.moveTo(x,y);
-    
+    ctx.moveTo(x,y);  
 })
-
 canvas.addEventListener("mousemove", function(e){
     if(mouseD==true)
     {
@@ -30,9 +26,7 @@ canvas.addEventListener("mousemove", function(e){
     ctx.lineTo(x,y);
     ctx.stroke();
     }
-    
 })
-
 canvas.addEventListener("mouseup", function(e){
     mouseD=false;
 })
