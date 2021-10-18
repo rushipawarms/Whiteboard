0623ca77e71e6ca84452ca3aa10584e9e41594d0
rushikesh Pawar <rushikesh.pawarms@gmail.com>
2021-10-18 10:18:44 +0530

    adding front end of pencil and eraser

--- tools.js
+++ tools.js
@@ -1,4 +1,55 @@
 let undo=document.querySelector("#undo");
+let redo=document.querySelector("#redo");
+
+let pencil=document.querySelector("#pencil");
+let eraser=document.querySelector("#eraser");
+let pencilOption=document.querySelector("#pencil-options");
+let eraserOption=document.querySelector("#eraser-options");
+console.log(eraserOption.classList);
+console.log(pencilOption.classList);
+
+pencil.addEventListener("click",function(){
+    if(!pencil.classList.contains("active-tool"))
+    {
+        eraser.classList.remove("active-tool");
+        pencil.classList.add("active-tool");
+        eraserOption.classList.add("hide");
+        ctx.strokeStyle="black";
+    }
+    else
+    {
+        if(pencilOption.classList.contains("hide"))
+        {
+            pencilOption.classList.remove("hide");
+        }
+        else{
+            pencilOption.classList.add("hide");
+        }
+        
+    }
+})
+eraser.addEventListener("click",function(){
+    if(!eraser.classList.contains("active-tool"))
+    {
+        pencil.classList.remove("active-tool");
+        eraser.classList.add("active-tool");
+        pencilOption.classList.add("hide");
+        ctx.strokeStyle="white";
+    }
+    else{
+        if(eraserOption.classList.contains("hide"))
+        {
+            eraserOption.classList.remove("hide");
+        }
+        else{
+            eraserOption.classList.add("hide");
+        }
+        
+
+    }
+    
+
+})
 
 undo.addEventListener("click",function(){
     if(pointsDB.length)
@@ -10,7 +61,7 @@ undo.addEventListener("click",function(){
     }
     
 })
-let redo=document.querySelector("#redo");
+
 redo.addEventListener("click",function(){
     if(redoA.length)
     {
